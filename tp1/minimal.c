f49667f0348b2dd076f4a74567d34607862be78e
Nicolas Senecal <nicolassenecal83@gmail.com>
2018-02-09 13:50:36 +0100

    exo6 - Ouverture de la palette

--- tp1/minimal.c
+++ tp1/minimal.c
@@ -16,29 +16,49 @@ static const Uint32 FRAMERATE_MILLISECONDS = 1000 / 60;
 
 /* Couleurs de dessins possibles */
 static const int COULEURS[][3] = {
-    {0, 0, 0},
-    {100, 100, 100},
-    {206, 206, 206},
-    {239, 239, 239},
     {255, 255, 255},
+    {239, 239, 239},
+    {206, 206, 206},
+    {100, 100, 100},
+    {0, 0, 0},
     {173, 57, 14},
     {30, 127, 203},
     {127, 221, 76},
     {167, 103, 38},
-    {136, 77, 167},
-    {230, 126, 48}
+    {136, 77, 167}
 };
 
 /* Nombre de couleurs */
-static const unsigned int NB_COULEURS = sizeof(COULEURS) / 3;
+static const unsigned int NB_COULEURS = sizeof (COULEURS) / (3 * sizeof (int));
+
+void selectColorView() {
+    int i;
+    glBegin(GL_QUADS);
+    for (i = 0; i < NB_COULEURS; ++i) {
+        glColor3ub(COULEURS[i][0], COULEURS[i][1], COULEURS[i][2]);
+
+        if (i % 2 == 0) {
+            glVertex2f(-1 + i * 2.f / NB_COULEURS, 1);
+            glVertex2f(-1 + (i + 2) * 2.f / NB_COULEURS, 1);
+            glVertex2f(-1 + (i + 2) * 2.f / NB_COULEURS, 0);
+            glVertex2f(-1 + i * 2.f / NB_COULEURS, 0);
+        } else {
+            glVertex2f(-1 + (i - 1) * 2.f / NB_COULEURS, -1);
+            glVertex2f(-1 + (i + 1) * 2.f / NB_COULEURS, -1);
+            glVertex2f(-1 + (i + 1) * 2.f / NB_COULEURS, 0);
+            glVertex2f(-1 + (i - 1) * 2.f / NB_COULEURS, 0);
+        }
+    }
+    glEnd();
+}
 
 int main(int argc, char** argv) {
 
-    int couleurActu = 1; /* numéro de la couleur par defaut */
-    char mode = 'p'; /* Mode de dessin ( p = points, l = lignes, t = triangle, c = couleur (menu) */
+    int couleurActu = 5; /* numéro de la couleur par defaut */
+    char mode = 0; /* Mode de dessin = 0, choix couleur =1 */
 
     /* Initialisation de la SDL */
-    if(-1 == SDL_Init(SDL_INIT_VIDEO)) {
+    if (-1 == SDL_Init(SDL_INIT_VIDEO)) {
         fprintf(stderr, "Impossible d'initialiser la SDL. Fin du programme.\n");
         return EXIT_FAILURE;
     }
@@ -47,7 +67,7 @@ int main(int argc, char** argv) {
     SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 0);
 
     /* Ouverture d'une fenêtre et création d'un contexte OpenGL */
-    if(NULL == SDL_SetVideoMode(WINDOW_WIDTH, WINDOW_HEIGHT, BIT_PER_PIXEL, SDL_OPENGL | SDL_GL_DOUBLEBUFFER | SDL_RESIZABLE )) {
+    if (NULL == SDL_SetVideoMode(WINDOW_WIDTH, WINDOW_HEIGHT, BIT_PER_PIXEL, SDL_OPENGL | SDL_GL_DOUBLEBUFFER | SDL_RESIZABLE)) {
         fprintf(stderr, "Impossible d'ouvrir la fenetre. Fin du programme.\n");
         return EXIT_FAILURE;
     }
@@ -61,7 +81,7 @@ int main(int argc, char** argv) {
 
     /* Boucle d'affichage */
     int loop = 1;
-    while(loop) {
+    while (loop) {
 
         /* Récupération du temps au début de la boucle */
         Uint32 startTime = SDL_GetTicks();
@@ -69,66 +89,74 @@ int main(int argc, char** argv) {
         /* Echange du front et du back buffer : mise à jour de la fenêtre */
         SDL_GL_SwapBuffers();
 
+        if (mode == 1) {
+            selectColorView();
+        } else {
+
+        }
+
         /* Boucle traitant les evenements */
         SDL_Event e;
-        while(SDL_PollEvent(&e)) {
+        while (SDL_PollEvent(&e)) {
 
 
             /* L'utilisateur ferme la fenêtre : */
-            if(e.type == SDL_QUIT) {
+            if (e.type == SDL_QUIT) {
                 loop = 0;
                 break;
             }
             /* Quelques exemples de traitement d'evenements : */
-            switch(e.type) {
+            switch (e.type) {
 
                     /*On redimensionne le viewport*/
                 case SDL_VIDEORESIZE:
                     WINDOW_WIDTH = e.resize.w;
-                    WINDOW_HEIGHT=e.resize.h;
-                    glViewport(0,0,WINDOW_WIDTH,WINDOW_HEIGHT);
+                    WINDOW_HEIGHT = e.resize.h;
+                    glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
                     glMatrixMode(GL_PROJECTION);
                     glLoadIdentity();
                     gluOrtho2D(-1., 1., -1., 1.);
-                    SDL_SetVideoMode(WINDOW_WIDTH, WINDOW_HEIGHT, BIT_PER_PIXEL, SDL_OPENGL | SDL_GL_DOUBLEBUFFER | SDL_RESIZABLE ); /* FIX ME - Écran en partie noir pendant le redimensionnement */
+                    SDL_SetVideoMode(WINDOW_WIDTH, WINDOW_HEIGHT, BIT_PER_PIXEL, SDL_OPENGL | SDL_GL_DOUBLEBUFFER | SDL_RESIZABLE); /* FIX ME - Écran en partie noir pendant le redimensionnement */
                     break;
 
 
-                /* Clic souris */
+                    /* Clic souris */
                 case SDL_MOUSEBUTTONUP:
                     /*
                     glClearColor((e.button.x/255)%255,((e.button.y/255)%255),0,1);
-                    */
-                    glBegin(GL_POINTS);
+                     */
+
+                    if (mode == 1) {
+                        
+                    } else {
+                        glBegin(GL_POINTS);
                         glColor3ub(255, 255, 255);
-                        glVertex2f(-1 + 2. * e.button.x / WINDOW_WIDTH, - (-1 + 2. * e.button.y / WINDOW_HEIGHT));
-                    glEnd();
-                    SDL_GL_SwapBuffers();
+                        glVertex2f(-1 + 2. * e.button.x / WINDOW_WIDTH, -(-1 + 2. * e.button.y / WINDOW_HEIGHT));
+                        glEnd();
+                        SDL_GL_SwapBuffers();
+                    }
+
                     break;
 
-                /* Mouvement de souris */
+                    /* Mouvement de souris */
                 case SDL_MOUSEMOTION:
                     /*
                     glClearColor((e.button.x%255)/255.0,((e.button.y%255)/255.0),0,1);
-                    */
+                     */
                     break;
 
-                /* Touche clavier */
+                    /* Touche clavier */
                 case SDL_KEYUP:
                     /* printf("touche pressée (code = %d)\n", e.key.keysym.sym); */
-
-                    switch(e.key.keysym.sym) {
+                    mode = 0;
+                    switch (e.key.keysym.sym) {
                         case SDLK_p:
-                            mode = 'p';
                             break;
                         case SDLK_l:
-                            mode = 'l';
                             break;
                         case SDLK_t:
-                            mode = 't';
                             break;
                         case SDLK_q:
-                            loop=0;
                             break;
                         default:
                             break;
@@ -136,8 +164,8 @@ int main(int argc, char** argv) {
                     break;
 
                 case SDL_KEYDOWN:
-                    if(e.key.keysym.sym == SDLK_SPACE) {
-                        mode = 'c';
+                    if (e.key.keysym.sym == SDLK_SPACE) {
+                        mode = 1;
                     }
                     break;
                 default:
@@ -149,13 +177,13 @@ int main(int argc, char** argv) {
         Uint32 elapsedTime = SDL_GetTicks() - startTime;
 
         /* Si trop peu de temps s'est écoulé, on met en pause le programme */
-        if(elapsedTime < FRAMERATE_MILLISECONDS) {
+        if (elapsedTime < FRAMERATE_MILLISECONDS) {
             SDL_Delay(FRAMERATE_MILLISECONDS - elapsedTime);
         }
     }
 
     /* Liberation des ressources associées à la SDL */
-        SDL_Quit();
+    SDL_Quit();
 
     return EXIT_SUCCESS;
 }
